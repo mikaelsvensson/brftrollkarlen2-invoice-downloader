c1deb65c1f0130e08b21999a55c75274de8b694f
Mikael Svensson <github@mikaelsvensson.info>
2016-12-27 08:34:29 +0100

    Documentation.

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
+Beskrivning
+===========
+
+Filen download.js är ett skript för CasperJS (webbläsare som kan köras i bakgrunden utan att några fönster visas)
+som laddar ner alla fakturor, för innevarande år, från Xpand (via Entré).
+
+Skriptet är utvecklat för brf. Trollkarlen 2.
+
+Vad skriptet gör:
+
+* Loggar in på Entré
+* Går till Webbattest
+* Söker fram fakturor
+* Sparar varje faktura lokalt på datorn
+* Sparar en översiktsfil som innehåller metadata för varje fil som sparats (filnamn, leverantör, belopp och datum)
+
 Installation
 ============
 
-Se http://docs.casperjs.org/en/latest/installation.html.
-
 1. Installera NodeJS (behöver PATH-variabeln uppdateras?)
 2. Installera PhantomJS (glöm inte att uppdatera PATH-variabeln)
-3. Installera CasperJS
-    $ npm install
+3. Ladda ner det här git-repot, antingen som en zip-fil eller mha. `git clone`
+4. Starta en kommandoprompt i mappen där skriptet finns och kör `npm install`
     
-Kör
-===
+Om något krånglar så kanske det finns användbara tips på http://docs.casperjs.org/en/latest/installation.html.
 
-$ node_modules\casperjs\bin\casperjs.exe download.js --username=YOUR_ENTRE_USERNAME --password=YOUR_ENTRE_PASSWORD
+Användning
+==========
+
+1. Starta en kommandoprompt och gå till mappen där du sparat skriptet.
+
+2. Kör detta kommando: `node_modules\casperjs\bin\casperjs.exe download.js --username=YOUR_ENTRE_USERNAME --password=YOUR_ENTRE_PASSWORD --directory=PATH_TO_PDF_FOLDER`
 
--- download.js
+++ download.js
@@ -1,20 +1,3 @@
-/**
- * Go to https://entre.stofast.se/
- * Type username into input[@name='Username']
- * Type password into input[@name='Password']
- * Click #loginSubmitButton
- * Wait for, and then click, .scrollFixLeftmenu a with text "Webbattest"
- * Switch to newly opened window
- * Click #ctl00_cphMainFrame_lbtSupplierInvoiceList
- * Select "18916 Trollkarlen 2 Brf" in #ctl00_cphMainFrame_SupplierInvoiceUC1_realEstateObjectSelectionUC_ddlCompanies
- * Wait for #ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_btnFilter to become enabled, and then click it
- * For each <tr> in #ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_gvInvoicesLr > tbody:
- * - Click the <tr>
- * - Click #ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_jqtInvoiceDocumentViewer
- * - Get src attribute from #ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_InvoiceDocumentViewer1_invoiceDocumentViewerIframe
- * - Download file referenced in src attr.
- */
-
 var fs = require('fs');
 
 var casper = require('casper').create({
@@ -41,17 +24,14 @@ function writeToFile(lines, filePath) {
 
 casper.start('https://entre.stofast.se');
 
+// Get command-line arguments:
 var username = casper.cli.options.username;
 var password = casper.cli.options.password;
 var outputFolder = casper.cli.options.directory || ".";
 
-casper.then(function () {
-    this.fill('form[action="/names.nsf?Login"]', {
-        Username: username,
-        Password: password
-    }, false);
-    this.click('input#loginSubmitButton');
-});
+/**
+ * Initialise some logging. Useful in case errors need to be debugged.
+ */
 
 //casper.on("popup.loaded", function (newPage) {
 //    console.log("Loaded " + newPage.url + " in popup window.");
@@ -73,18 +53,47 @@ casper.on("page.error", function (msg, backtrace) {
     this.echo("=========================");
 });
 
+/**
+ * Log in to Entre.
+ */
+casper.then(function () {
+    this.fill('form[action="/names.nsf?Login"]', {
+        Username: username,
+        Password: password
+    }, false);
+    this.click('input#loginSubmitButton');
+});
+
+/**
+ * Wait for link to Webbattest becomes visible, and then click it.
+ */
 casper.waitForSelector('.scrollFixLeftmenu', function () {
     this.click('.scrollFixLeftmenu a[title*="Webbattest"]');
 });
 
+/**
+ * Wait for Webbattest to be opened in new window.
+ */
 casper.waitForPopup('http://xpandwebb.stofast.se');
 
+/**
+ * Do the following in the Webbattest window:
+ */
 casper.withPopup(/xpandwebb.stofast.se/, function () {
+    /**
+     * Wait for the "search for supplier invoices link" becomes visible...
+     */
     this.waitForSelector('#ctl00_cphMainFrame_lbtSupplierInvoiceList', function () {
+        /**
+         * ...and click it.
+         */
         this.click('#ctl00_cphMainFrame_lbtSupplierInvoiceList');
 
         this.waitForUrl(/SupplierInvoiceListSearch/);
 
+        /**
+         * Select company from the drop-down list...
+         */
         this.waitForSelector('#ctl00_cphMainFrame_SupplierInvoiceUC1_realEstateObjectSelectionUC_ddlCompanies', function () {
             this.evaluate(function (index) {
                 var form = document.querySelector('#ctl00_cphMainFrame_SupplierInvoiceUC1_realEstateObjectSelectionUC_ddlCompanies');
@@ -93,7 +102,13 @@ casper.withPopup(/xpandwebb.stofast.se/, function () {
             }, 1);
         });
 
+        /**
+         * ...and wait for the page to reload.
+         */
         this.wait(5000, function () {
+            /**
+             * Click search button:
+             */
             this.click('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_btnFilter');
         });
 
@@ -104,6 +119,7 @@ casper.withPopup(/xpandwebb.stofast.se/, function () {
 
             const HEADER_ROW_COUNT = 1;
 
+            // Calculate the number of invoice table rows that were found
             var rows = this.evaluate(function () {
                 return document.querySelectorAll('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_gvInvoicesLr tbody tr').length;
             });
@@ -112,13 +128,22 @@ casper.withPopup(/xpandwebb.stofast.se/, function () {
 
             var currentRow = rows;//Math.min(rows, 51);
 
+            /**
+             * Used to store invoice metadata which will be saved to file at the end.
+             */
             var invoicesMetadata = [];
 
+            // Add headers to metadata file contents.
             invoicesMetadata.push(["FIL", "LEVERANTOR", "REFERENS", "BELOPP", "DATUM"].join("\t"));
 
+            /**
+             * Recursive function for processing rows.
+             */
             var processNextRow = function () {
 
+                // Exit condition:
                 if (currentRow <= HEADER_ROW_COUNT) {
+                    writeToFile.call(this, invoicesMetadata, outputFolder + "/invoices-" + getDate() + ".tsv");
                     return;
                 }
 
@@ -141,19 +166,27 @@ casper.withPopup(/xpandwebb.stofast.se/, function () {
                 invoicesMetadata.push([targetFile, supplierName, supplierInvoiceReference, amount, invoiceDate].join("\t"));
 
                 if (!fs.exists(targetPath)) {
+                    /**
+                     * Click row in invoice overview tab:
+                     */
                     this.click('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_gvInvoicesLr tbody tr:nth-child(' + (currentRow) + ')')
                     this.wait(3000, function () {
+                        /**
+                         * Clicking the row switches to the second tab, but we want the third one:
+                         */
                         this.click('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_jqtInvoiceDocumentViewer');
                         this.wait(3000, function () {
+
+                            var link = this.getElementAttribute('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_InvoiceDocumentViewer1_invoiceDocumentViewerIframe', 'src');
+                            this.echo("Downloading " + link + " to " + targetPath);
+                            this.download(link, targetPath);
+
+                            // Decrease row counter, in preparation for next call to processNextRow.
+                            currentRow--;
+
+                            // Click on the first tab, the overview tab, and wait for that tab to load:
                             this.click('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_jqtInvoiceList');
-                            this.wait(3000, function () {
-                                //this.capture('homepage-' + getDate() + '-invoice.png');
-                                var link = this.getElementAttribute('#ctl00_cphMainFrame_SupplierInvoiceUC1_jqTabs_InvoiceDocumentViewer1_invoiceDocumentViewerIframe', 'src');
-                                this.echo("Downloading " + link + " to " + targetPath);
-                                this.download(link, targetPath);
-                                currentRow--;
-                                this.wait(3000, processNextRow);
-                            });
+                            this.wait(3000, processNextRow);
                         });
                     });
                 } else {
@@ -163,9 +196,8 @@ casper.withPopup(/xpandwebb.stofast.se/, function () {
                 }
             };
 
+            // Process first row (actually the last invoice in the search result since we start from the end):
             processNextRow.apply(this);
-
-            writeToFile.call(this, invoicesMetadata, outputFolder + "/invoices-" + getDate() + ".tsv");
         });
     });
 });
